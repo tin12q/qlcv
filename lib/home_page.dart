e53daa515d9e607350bec937246d7bb3998f26c1
vkutin12q <thinhnp.22git@vku.udn.vn>
2023-02-23 17:12:02 +0700

    update navbar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -14,16 +14,12 @@ import 'route/calendar.dart';
 import 'route/menu.dart';
 import 'package:qlcv/model/color_picker.dart';
 class HomePage extends StatefulWidget {
-
-  void loadData() async
-  {
-    await DBHelper.taskUpdate();
-  }
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  bool _dataLoaded = false;
   int _selectedIndex = 1;
 
   final List<Widget> _widgetOptions = <Widget>[
@@ -31,75 +27,89 @@ class _HomePageState extends State<HomePage> {
     Home(),
     Calendar(),
     const Menu(),
-
   ];
 
-  //future build
-  @override
-  initState() {
-    super.initState();
-    widget.loadData();
+
+  Future<void> loadData() async {
+    if (!_dataLoaded) {
+      await DBHelper.taskUpdate();
+      setState(() {
+        _dataLoaded = true;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return
-      Scaffold(
-        body: _widgetOptions.elementAt(_selectedIndex),
-        bottomNavigationBar: Container(
-            color: ColorPicker.primary,
-            child: SafeArea(
-              child: GNav(
-                gap: 5,
-                activeColor: ColorPicker.primary,
-                color: ColorPicker.dark,
-                rippleColor: ColorPicker.primary,
-                hoverColor: ColorPicker.accent,
-                iconSize: 19,
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 30, vertical: 20),
-                duration: const Duration(milliseconds: 300),
-                tabBackgroundColor: ColorPicker.accent,
-                backgroundColor: ColorPicker.primary,
-
-                tabs: _tabs,
-                selectedIndex: 1,
-                onTabChange: (index) {
-                  setState(() {
-                    _selectedIndex = index;
-                  });
-                },
-              ),
-            )
-        ));
+    if (!_dataLoaded) {
+      loadData();
+      return Center(
+        child: CircularProgressIndicator(),
+      );
     }
-    final _tabs = (Platform.isIOS) ? const [
-      GButton(icon: CupertinoIcons.square_grid_2x2),
-      GButton(icon: CupertinoIcons.home,
-        text: 'Home',),
-      GButton(icon: CupertinoIcons.calendar_today
-        , text: 'Calendar',),
-      GButton(icon: CupertinoIcons.bars
-        , text: 'Menu',),
-
-    ] : const [
-      GButton(
-        icon: Icons.dashboard_outlined,
+    return Scaffold(
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: Container(
+        color: ColorPicker.primary,
+        child: SafeArea(
+          child: GNav(
+            gap: 5,
+            activeColor: ColorPicker.primary,
+            color: ColorPicker.dark,
+            rippleColor: ColorPicker.primary,
+            hoverColor: ColorPicker.accent,
+            iconSize: 19,
+            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+            duration: const Duration(milliseconds: 300),
+            tabBackgroundColor: ColorPicker.accent,
+            backgroundColor: ColorPicker.primary,
+            tabs: _tabs,
+            selectedIndex: 1,
+            onTabChange: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+          ),
+        ),
       ),
-      GButton(
-        icon: Icons.home_outlined,
-        text: 'Home',
+    );
+  }
 
-      ),
-      GButton(
-        icon: Icons.calendar_month_outlined,
-        text: 'Calendar',
-
-      ),
-      GButton(
-        icon: Icons.menu_outlined,
-        text: 'Menu',
-
-      )
-    ];
+  final _tabs = (Platform.isIOS)
+      ? const [
+    GButton(icon: CupertinoIcons.square_grid_2x2,
+      iconSize: 25,),
+    GButton(
+      icon: CupertinoIcons.home,
+      iconSize: 25,
+      text: 'Home',
+    ),
+    GButton(
+      icon: CupertinoIcons.calendar_today,
+      iconSize: 25,
+      text: 'Calendar',
+    ),
+    GButton(
+      icon: CupertinoIcons.bars,
+      iconSize: 25,
+    ),
+  ]
+      : const [
+    GButton(
+      icon: Icons.dashboard_outlined,
+    ),
+    GButton(
+      icon: Icons.home_outlined,
+      text: 'Home',
+    ),
+    GButton(
+      icon: Icons.calendar_month_outlined,
+      text: 'Calendar',
+    ),
+    GButton(
+      icon: Icons.menu_outlined,
+      text: 'Menu',
+    )
+  ];
 }
